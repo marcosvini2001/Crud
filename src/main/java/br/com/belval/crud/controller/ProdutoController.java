f214da60fb4443c00bac17d5cf7b1071615d59e2
Marcos <rm91931@estudante.fieb.edu.br>
2023-09-15 21:58:09 -0300

    Add contador proxId e lista

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -1,13 +1,21 @@
 package br.com.belval.crud.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import br.com.belval.crud.model.Produto;
 
 @Controller
 public class ProdutoController {
+	
+	private static List<Produto> lista = new ArrayList<>();
+	private static int proxId = 1;
+	
 	@GetMapping("/produto/novo")
 	public String novo() {
 		return "novo-produto";
@@ -16,6 +24,9 @@ public class ProdutoController {
 	@PostMapping("/produto/novo")
 	public ModelAndView novo(Produto produto) {
 		ModelAndView modelAndView = new ModelAndView("novo-produto-criado");
+		
+		produto.setId(++proxId);
+		
 		modelAndView.addObject("novoProduto", produto);
 		return modelAndView;
 	}
