9748b2a70fbce3e6e1724fe10b333b01c7482fb8
Marcos <rm91931@estudante.fieb.edu.br>
2023-10-06 21:05:39 -0300

    Agora, apos inserir ou atualizar um produto vai para a lista e mostra a mensagem de confirmação

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package br.com.belval.crud.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,58 +10,98 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.belval.crud.model.Produto;
-
 
 @Controller
 public class ProdutoController {
 	
 	private static List<Produto> lista = new ArrayList<>();
 	private static int proxId = 1;
-	
-	@GetMapping("/produto/novo")
-	public String novo() {
-		return "novo-produto";
-	}
 
+	@GetMapping("/produto/novo")
+	public String novo(Model model) {
+		model.addAttribute("produto", new Produto());
+		return "produto";
+	}
+	
+	@GetMapping("/produto/{id}/edit")
+	public String editar(@PathVariable int id, Model model) {
+		
+		Produto produto = buscarPorId(id);
+		
+		if (produto == null) {
+			return "produto-nao-encontrado";
+		}
+		
+		model.addAttribute("produto", produto);
+		
+		return "produto";
+	}
+	
 	@PostMapping("/produto/novo")
-	public ModelAndView novo(Produto produto) {
-		ModelAndView modelAndView = new ModelAndView("novo-produto-criado");
+	public ModelAndView novo(Produto produto, RedirectAttributes redirectAttributes) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/produto/list");
 		
-		produto.setId(proxId++);
+		if (produto.getId() == 0) {
+			insert(produto);
+			redirectAttributes.addFlashAttribute("msg","Novo produto criado!");
+		} else {
+			update(produto);
+			redirectAttributes.addFlashAttribute("msg","Produto atualizado!");
+		}
 		
-		lista.add(produto);
-		
-		modelAndView.addObject("novoProduto", produto);
 		return modelAndView;
 	}
+
+	private void insert(Produto produto) {
+		produto.setId(proxId++);
+		lista.add(produto);
+	}
+
+	private void update(Produto produto) {
+		ListIterator<Produto> it = lista.listIterator();
+		while(it.hasNext()) {
+			Produto encontrado = it.next();
+			if (encontrado.getId() == produto.getId()) {
+				it.remove();
+				it.add(produto);
+			}
+		}
+	}
+
 	@GetMapping("/produto/list")
 	public String list(Model model) {
-		model.addAttribute("produtos", lista);  
+		model.addAttribute("produtos", lista);
 		return "lista-produtos";
 	}
-	 
+	
 	@GetMapping("/produto/{id}")
 	public String detalhe(@PathVariable int id, Model model) {
 		Produto produto = buscarPorId(id);
+		
 		if (produto != null) {
-			model.addAttribute("novoProduto",produto);
+			model.addAttribute("novoProduto", produto);
 			return "novo-produto-criado";
 		}
 		
-		return "produto-não-encontrado"; 
+		return "produto-nao-encontrado";
 	}
 
 	private Produto buscarPorId(int id) {
 		Produto encontrou = null;
 		for(Produto p : lista) {
-			if(p.getId() == id) {
-				// encontrou o produto solicitado
+			if (p.getId() == id) {
+				//encontrou o produto solicitado
 				encontrou = p;
 				break;
 			}
 		}
 		return encontrou;
 	}
+	
+	
+
+	
 }
